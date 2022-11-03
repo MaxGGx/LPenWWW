366177930dc507cc5c1fcf88de356f48fb46b962
Bitovito <vicente.alvarez.aguayo@gmail.com>
2022-11-03 13:44:12 -0300

    Primer commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Drawer from './components/Drawer'
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <Drawer/>
+      </div>
+    );
+  }
+}
+
+export default App;
